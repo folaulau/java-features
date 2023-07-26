c658231324c5077377ab9d8f6a437ed0d5f08d5d
Folau Kaveinga <folaukaveinga@gmail.com>
2023-07-25 18:53:49 -0600

    added method reference

--- src/main/java/com/folautech/java/User.java
+++ src/main/java/com/folautech/java/User.java
@@ -2,9 +2,13 @@ package com.folautech.java;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Data
 public class User implements Serializable {
---
+++ src/main/java/com/folautech/java8/Java8MethodReference.java
@@ -0,0 +1,111 @@
+package com.folautech.java8;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import com.folautech.java.Student;
+import com.folautech.java.User;
+import com.github.javafaker.Faker;
+
+public class Java8MethodReference {
+
+    static Faker faker = new Faker();
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Java8MethodReference");
+
+        staticMethodReference();
+
+        instanceMethodReference();
+
+        constructorMethodReference();
+    }
+
+    /**
+     * a reference to a static method is a type of method reference that allows you to reference and use a static method
+     * of a class as a functional interface. It is denoted by ClassName::staticMethodName. This type of method reference
+     * is useful when you want to pass a static method as an argument to a functional interface or use it in lambda
+     * expressions.
+     * 
+     * ClassName::staticMethodName
+     * 
+     * 
+     */
+    static void staticMethodReference() {
+        System.out.println("\nstaticMethodReference...");
+
+        // Using lambda expression
+        Arrays.asList("apple", "banana", "orange").forEach(str -> System.out.println(str.toUpperCase()));
+
+        // Using method reference
+        Arrays.asList("apple", "banana", "orange").forEach(Java8MethodReference::convertToUpperCase);
+
+        System.out.println("staticMethodReference done!");
+    }
+
+    public static void convertToUpperCase(String str) {
+        System.out.println(str.toUpperCase());
+    }
+
+    /***
+     * a reference to an instance method is a type of method reference that allows you to reference and use an instance
+     * method of a particular object as a functional interface. It is denoted by instance::instanceMethodName. This type
+     * of method reference is useful when you want to pass an instance method as an argument to a functional interface
+     * or use it in lambda expressions.
+     * 
+     * instance::instanceMethodName
+     * 
+     */
+    static void instanceMethodReference() {
+        System.out.println("\ninstanceMethodReference...");
+
+        List<String> fruits = Arrays.asList("apple", "banana", "orange");
+
+        // Using lambda expression
+        fruits.forEach(str -> {
+            System.out.println("str: " + str + ",length: " + str.length());
+        });
+
+        Java8MethodReference java8MethodReference = new Java8MethodReference();
+
+        // Using method reference
+        fruits.forEach(java8MethodReference::printLength);
+
+        System.out.println("instanceMethodReference done!");
+    }
+
+    public void printLength(String str) {
+        System.out.println("str: " + str + ",length: " + str.length());
+    }
+
+    /**
+     * a reference to a constructor is a type of method reference that allows you to reference and use a constructor of
+     * a class as a functional interface. It is denoted by ClassName::new. This type of method reference is useful when
+     * you want to pass a constructor as an argument to a functional interface or use it in lambda expressions.
+     * 
+     * ClassName::new
+     * 
+     */
+    static void constructorMethodReference() {
+        System.out.println("\nconstructorMethodReference...");
+
+        // Using lambda expression
+        Supplier<User> personLambda = () -> User.builder().firstName("John").lastName("Doe").build();
+        User johnFromLambda = personLambda.get();
+        System.out.println("User from lambda: " + johnFromLambda);
+
+        // Using method reference
+        Supplier<User> personMethodRef = User::new;
+        User johnFromMethodRef = personMethodRef.get();
+        System.out.println("Person from method reference: " + johnFromMethodRef);
+
+        System.out.println("constructorMethodReference done!");
+    }
+
+}
